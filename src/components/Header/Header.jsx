8d36932610aa9504323335763ff35e878d637fc7
rodrothbarth <rodrothbarth@gmail.com>
2024-09-02 18:08:42 -0300

    CSS introduction for tailwind

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
-import logo from '../../assets/logo.png';
+import logo from "../../assets/logo.png";
 
-import {StyleHeader} from "./styleHeaderComponent.js";
 export default function Header() {
   return (
-    <StyleHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img
+        src={logo}
+        alt="A canvas"
+        className="mb-8 w-44 h-44 object-contain"
+      />
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+        ReactArt
+      </h1>
       {/*<p className={classes.paragraph}>A community of artists and art-lovers.</p>*/}
-      <p>A community of artists and art-lovers.</p>
-    </StyleHeader>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
+    </header>
   );
 }
